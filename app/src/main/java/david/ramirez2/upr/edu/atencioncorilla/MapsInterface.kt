fc1e9b4acc7416b2e275e6af344a3ddbb81e6b41
David Ramirez <dramirezgomez20@gmail.com>
2018-10-27 19:08:00 -0400

    Added Navigation Interface, changed Map Activity to Map Fragment

--- app/src/main/java/david/ramirez2/upr/edu/atencioncorilla/MapsInterface.kt
+++ app/src/main/java/david/ramirez2/upr/edu/atencioncorilla/MapsInterface.kt
@@ -1,10 +1,18 @@
 package david.ramirez2.upr.edu.atencioncorilla
 
+import android.app.Dialog
+import android.content.DialogInterface
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
+import android.support.v4.app.Fragment
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.text.InputType
+import android.text.InputType.TYPE_CLASS_TEXT
+import android.view.View
+import android.widget.EditText
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -19,6 +27,8 @@ class MapsInterface : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var lastLocation: Location
+   // private lateinit var builder: AlertDialog.Builder
+
 
     companion object {
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1
@@ -45,6 +55,14 @@ class MapsInterface : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        //var input = EditText(this)
+
+      //  builder.setTitle("Enter Report Information")
+
+      //  input.inputType = InputType.TYPE_CLASS_TEXT
+      //  builder.setView(input)
+
     }
 
     /**
@@ -57,15 +75,21 @@ class MapsInterface : AppCompatActivity(), OnMapReadyCallback {
      * installed Google Play services and returned to the app.
      */
     override fun onMapReady(googleMap: GoogleMap) {
+
         mMap = googleMap
         mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+
+        //builder = dialog
+        //builder.setTitle("Hello")
+
+        /*
 
         mMap.setOnMapClickListener{point: LatLng ->
 
             mMap.addMarker(MarkerOptions().position(point).title("Testing"))
             //   mMap.addMarker(MarkerOptions().)
 
-        }
+        }*/
 
         //  setPermission()
 
@@ -105,5 +129,59 @@ class MapsInterface : AppCompatActivity(), OnMapReadyCallback {
           mMap.addMarker(MarkerOptions().position(sydney2).title("Marker in Sydney"))*/
 
         // mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 12.0f))
+    }
+
+    fun reportPin(view: View) {
+
+        //val input = EditText(this)
+      //  builder.setView(input)
+
+       // val input = EditText(this)
+       // builder.setView(input)
+
+        // If the button clicked is the "Report" button
+
+        if(view.id == R.id.report) {
+
+            // If button clicked, turn on the Map click listener to allow user to
+            // set a report pin on the map
+
+            mMap.setOnMapClickListener {point: LatLng ->
+
+                val dialog = AlertDialog.Builder(this)
+                val input = EditText(this)
+                input.inputType = TYPE_CLASS_TEXT
+                dialog.setView(input)
+
+                dialog.setTitle("Confirm Report")
+                        .setMessage("Confirm!")
+                        .setPositiveButton("OK", {dialog, i ->
+                            val description = input.text.toString()
+                            mMap.addMarker(MarkerOptions().position(point).title(description))
+                        })
+                        .setNegativeButton("Cancel",{dialog, which ->
+                        })
+
+                dialog.show()
+
+               /* builder.setPositiveButton("OK"){dialog: DialogInterface, which: Int ->
+
+                    val description = input.text.toString()
+                    mMap.addMarker(MarkerOptions().position(point).title(description))
+                }
+
+                builder.setNegativeButton("Cancel"){dialog: DialogInterface, which: Int ->
+                    dialog.cancel()
+                }
+
+                builder.show()*/
+                //   mMap.addMarker(MarkerOptions().)
+                //if(view.id == R.id.report)
+
+                // After report is added, turn the Map click listener off
+
+                mMap.setOnMapClickListener(null)
+            }
+        }
     }
 }
